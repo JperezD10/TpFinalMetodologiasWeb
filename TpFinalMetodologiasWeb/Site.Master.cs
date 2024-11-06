16c9774bc01677000135be5509c544571d9c3511
JPerezD10 <jperezdemonty@live.com.ar>
2024-11-05 21:17:38 -0300

    Logout and log

--- TpFinalMetodologiasWeb/Site.Master.cs
+++ TpFinalMetodologiasWeb/Site.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BE;
+using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,28 @@ namespace TpFinalMetodologiasWeb
 {
     public partial class SiteMaster : MasterPage
     {
+        BLLBitacora bLLBitacora;
+        public SiteMaster()
+        {
+            bLLBitacora = new BLLBitacora();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void LogoutButton_Click(object sender, EventArgs e)
+        {
+            var usuario = (BEUsuario)Session["Usuario"];
+            bLLBitacora.RegistrarBitacora(new BEBitacora
+            {
+                Fecha = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"),
+                Modulo = "Logout",
+                Movimiento = $"Cierre de sesion del usuario",
+                Usuario = usuario.Usuario
+            });
+            Session["Usuario"] = null;
+            Response.Redirect("Login.aspx");
         }
     }
 }
